e9c92b71c56aa8de175be68346b91d7fbbb11809
Lukas <mr.halleg@gmail.com>
2020-07-08 18:24:21 +0200

    added retrysearchresulthandler

---
+++ src/main/java/halleg/discordmusikbot/guild/youtube/RetryYoutubeSearchProvider.java
@@ -0,0 +1,26 @@
+package halleg.discordmusikbot.guild.youtube;
+
+import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeSearchProvider;
+import com.sedmelluq.discord.lavaplayer.track.AudioItem;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
+
+import java.util.function.Function;
+
+public class RetryYoutubeSearchProvider extends YoutubeSearchProvider {
+    @Override
+    public AudioItem loadSearchResult(String query, Function<AudioTrackInfo, AudioTrack> trackFactory) {
+        int i = 8;
+
+        while (true) {
+            try {
+                return super.loadSearchResult(query, trackFactory);
+            } catch (Exception e) {
+                i--;
+                if (i < 0) {
+                    throw e;
+                }
+            }
+        }
+    }
+}
