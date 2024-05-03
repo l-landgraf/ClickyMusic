bfbbc0b1479e9ee3290dc4605693769e0942ad08
Lukas <mr.halleg@gmail.com>
2024-05-03 11:56:15 +0200

    added disconnect listener

--- src/main/java/halleg/discordmusikbot/DisconnectListener.java
+++ src/main/java/halleg/discordmusikbot/DisconnectListener.java
@@ -1,6 +1,12 @@
 package halleg.discordmusikbot;
 
+import net.dv8tion.jda.api.events.session.SessionDisconnectEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class DisconnectListener extends ListenerAdapter {
+    @Override
+    public void onSessionDisconnect(SessionDisconnectEvent event) {
+        System.out.println("Disconnected. shutting down.");
+        System.exit(1);
+    }
 }
