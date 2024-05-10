47084a1bd4b300e99002d1fca4205fd2b2d3d692
Lukas <mr.halleg@gmail.com>
2024-05-10 19:09:42 +0200

    added timer to shutdown on disconnect

--- src/main/java/halleg/discordmusikbot/DisconnectListener.java
+++ src/main/java/halleg/discordmusikbot/DisconnectListener.java
@@ -1,12 +1,30 @@
 package halleg.discordmusikbot;
 
+import halleg.discordmusikbot.guild.player.Timer;
 import net.dv8tion.jda.api.events.session.SessionDisconnectEvent;
+import net.dv8tion.jda.api.events.session.SessionResumeEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class DisconnectListener extends ListenerAdapter {
+public class DisconnectListener extends ListenerAdapter implements Timer.TimerListener {
+    private Timer timer;
+
+    public DisconnectListener() {
+        this.timer = new Timer(30, false, this);
+    }
+
+    @Override
+    public void onSessionResume(SessionResumeEvent event) {
+        this.timer.stop();
+    }
+
     @Override
     public void onSessionDisconnect(SessionDisconnectEvent event) {
-        System.out.println("Disconnected. shutting down.");
+        this.timer.start();
+    }
+
+    @Override
+    public void onTimerEnd() {
+        System.out.println("Disconnect timer run out. shutting down.");
         System.exit(1);
     }
 }
